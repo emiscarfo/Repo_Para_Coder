a64ba5f3867b67ed129519488023bf329df1bc7b
emiscarfo <emiscarfo@hotmail.com>
2022-11-17 18:55:33 -0300

    con vista saludo, fecha y admin

--- Proye_Empresa/multiserviceSRL/multiserviceSRL/urls.py
+++ Proye_Empresa/multiserviceSRL/multiserviceSRL/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from multiserviceSRL.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista-saludos/', vista_saludos),
+    path('fecha/', dia_hoy)
 ]
